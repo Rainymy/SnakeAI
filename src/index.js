91f67d2cd137efb01227e9e8b2f8b84ca8ca1614
Rainymy <sstrike@yahoo.com>
2020-08-25 22:25:56 +0200

    init
    Initialization

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isElectronDev = require('electron-is-dev');
+
+if (require('electron-squirrel-startup')) { 
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      worldSafeExecuteJavaScript: true
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  
+  mainWindow.onbeforeunload = (e) => {
+    console.log("hello");
+    e.preventDefault();
+    e.returnValue = false;
+    return false;
+  }
+  // Open the DevTools.
+  if (isElectronDev) { mainWindow.webContents.openDevTools(); }
+};
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') { app.quit(); }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) { createWindow(); }
+});
