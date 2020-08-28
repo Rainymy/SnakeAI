9abaa3f002cd599a6edd9b7218ed50451487991e
Rainymy <48211766+Rainymy@users.noreply.github.com>
2020-08-28 16:35:55 +0200

    Update renderer.js

--- src/JS/renderer.js
+++ src/JS/renderer.js
@@ -1,21 +1,24 @@
-function propsObject() {
+function propsObject(boxes) {
   this.canvas = document.getElementById("canvas");
   this.ctx = this.canvas.getContext("2d");
+  this.totalBoxes = boxes || 1;
+  this.boxPixel = this.canvas.width / this.totalBoxes;
   this.drawSqure = (x, y, size) => {
     this.ctx.beginPath();
     this.ctx.rect(x, y, size, size);
     this.ctx.stroke();
   };
-}
-let game = new propsObject();
-
-let totalBoxes = 20;
-let boxPixel = game.canvas.width / totalBoxes;
-
-for (let i = 0; i < game.canvas.width/boxPixel; i++) {
-  for (let j = 0; j < game.canvas.height/boxPixel; j++) {
-    game.drawSqure(i * boxPixel, j * boxPixel, boxPixel);
+  this.drawMap = () => {
+    for (let i = 0; i < this.canvas.width/this.boxPixel; i++) {
+      for (let j = 0; j < this.canvas.height/this.boxPixel; j++) {
+        this.drawSqure(i * this.boxPixel, j * this.boxPixel, this.boxPixel);
+      }
+    }
   }
 }
-console.log(boxPixel);
+let totalRowBoxes = 99;
+let game = new propsObject(totalRowBoxes);
+
+game.drawMap()
+
 console.log(game);
