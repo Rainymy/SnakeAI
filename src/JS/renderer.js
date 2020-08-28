485a9c056393383c1d65aff47cfd94f806e7907c
Rainymy <48211766+Rainymy@users.noreply.github.com>
2020-08-28 16:23:06 +0200

    .

--- src/JS/renderer.js
+++ src/JS/renderer.js
@@ -1,1 +1,21 @@
-console.log("hello");
+function propsObject() {
+  this.canvas = document.getElementById("canvas");
+  this.ctx = this.canvas.getContext("2d");
+  this.drawSqure = (x, y, size) => {
+    this.ctx.beginPath();
+    this.ctx.rect(x, y, size, size);
+    this.ctx.stroke();
+  };
+}
+let game = new propsObject();
+
+let totalBoxes = 20;
+let boxPixel = game.canvas.width / totalBoxes;
+
+for (let i = 0; i < game.canvas.width/boxPixel; i++) {
+  for (let j = 0; j < game.canvas.height/boxPixel; j++) {
+    game.drawSqure(i * boxPixel, j * boxPixel, boxPixel);
+  }
+}
+console.log(boxPixel);
+console.log(game);
