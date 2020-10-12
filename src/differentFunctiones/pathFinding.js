a1750b40d08d2bfcb0e7fbd7bacbc8810a12f869
Rainymy <48211766+Rainymy@users.noreply.github.com>
2020-10-12 17:12:04 +0200

    Update pathFinding.js

--- src/differentFunctiones/pathFinding.js
+++ src/differentFunctiones/pathFinding.js
@@ -1,4 +1,5 @@
 const aStar = new function () {
+  "use strict";
   const Initial = {
     FOOD: -1,
     WALL: 1,
@@ -7,56 +8,36 @@ const aStar = new function () {
   }
   this.mapGrid = null;
   this.mapArrayWithPosition = null;
+  this.getObjectArray = function(options, defaultValue) {
+    return Object.assign({
+      isWall: false,
+      isFood: false, 
+      isHead: false
+    }, options, defaultValue);
+  }
   this.convertGridToArray = function(list, walls, foods) {
     let grid = [];
     let last = null;
-    outer: for (let [index, current] of list.entries()) {
+    outer: for (let current of list) {
       if (!last || current.x !== last.x) {
         last = current;
         grid.push([]);
       }
-      for (let [k, food] of foods.entries()) {
+      for (let food of foods) {
         if (food.x === current.x && food.y === current.y) {
-          if (k === 1) {
-            grid[grid.length - 1].push(
-              Object.assign({
-                isWall: false,
-                isFood: true,
-                isHead: true
-              }, current)
-            );
-          }
-          else {
-            grid[grid.length - 1].push(
-              Object.assign({
-                isWall: false, 
-                isFood: true,
-                isHead: false 
-              }, current)
-            );
-          }
+          grid[grid.length - 1].push(this.getObjectArray({ isFood: true }, current));
           continue outer;
         }
       }
       for (let [j, wall] of walls.entries()) {
         if (j !== 0 && wall.x === current.x && wall.y === current.y) {
           grid[grid.length - 1].push(
-            Object.assign({
-              isWall: true,
-              isFood: false,
-              isHead: false 
-            }, current)
+            this.getObjectArray(j === 1 ? {isHead: true}: {isWall: true}, current)
           );
           continue outer;
         }
       }
-      grid[grid.length - 1].push(
-        Object.assign({
-          isWall: false, 
-          isFood: false,
-          isHead: false 
-        }, current)
-      )
+      grid[grid.length - 1].push(this.getObjectArray(current));
     }
     return grid;
   }
@@ -70,18 +51,10 @@ const aStar = new function () {
           grid.push([]);
         }
         
-        if (obj.isHead) {
-          grid[grid.length - 1].push(Initial.HEAD);
-        } 
-        else if (obj.isWall) {
-          grid[grid.length - 1].push(Initial.WALL);
-        } 
-        else if (obj.isFood) {
-          grid[grid.length - 1].push(Initial.FOOD);
-        } 
-        else {
-          grid[grid.length - 1].push(Initial.AIR);
-        }
+             if (obj.isHead) { grid[grid.length - 1].push(Initial.HEAD); } 
+        else if (obj.isWall) { grid[grid.length - 1].push(Initial.WALL); } 
+        else if (obj.isFood) { grid[grid.length - 1].push(Initial.FOOD); } 
+        else                 { grid[grid.length - 1].push(Initial.AIR); }
       }
     }
     return grid;
@@ -89,13 +62,26 @@ const aStar = new function () {
   this.findPathFromTo = function (snakeHead, food) {
     return null;
   }
+  this.getNearestFood = function ( objects ) {
+    return objects;
+  }
+  this.getAllObjectLocation = function() {
+    let obj = { head: [], food: [] };
+    for (let [i, x] of this.mapGrid.entries()) {
+      for (let [j, y] of x.entries()) {
+        if (y === Initial.HEAD) { obj.head = { row: i, column: j }; }
+        else if (y === Initial.FOOD) { obj.food.push({ row: i, column: j }); }
+      }
+    }
+    return obj;
+  }
   this.search = function(obj, map) {
     if (!this.mapGrid) {
       let start = performance.now();
       this.mapArrayWithPosition = this.convertGridToArray(map, obj.bodies, obj.foods);
       this.mapGrid = this.createGridFromArray(this.mapArrayWithPosition);
     }
-    console.log(this.mapGrid);
+    this.getAllObjectLocation();
   }
 }
 
