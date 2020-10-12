d8ada9eaa20c8b4f09e650bbca10fe6da202be5f
Rainymy <48211766+Rainymy@users.noreply.github.com>
2020-10-12 11:37:17 +0200

    Update pathFinding.js

--- src/differentFunctiones/pathFinding.js
+++ src/differentFunctiones/pathFinding.js
@@ -2,6 +2,7 @@ const aStar = new function () {
   const Initial = {
     FOOD: -1,
     WALL: 1,
+    HEAD: 2,
     AIR: 0
   }
   this.mapGrid = null;
@@ -16,23 +17,46 @@ const aStar = new function () {
       }
       for (let [k, food] of foods.entries()) {
         if (food.x === current.x && food.y === current.y) {
-          grid[grid.length - 1].push(
-            Object.assign({ isWall: false, isFood: true }, current)
-          );
+          if (k === 1) {
+            grid[grid.length - 1].push(
+              Object.assign({
+                isWall: false,
+                isFood: true,
+                isHead: true
+              }, current)
+            );
+          }
+          else {
+            grid[grid.length - 1].push(
+              Object.assign({
+                isWall: false, 
+                isFood: true,
+                isHead: false 
+              }, current)
+            );
+          }
           continue outer;
         }
       }
       for (let [j, wall] of walls.entries()) {
         if (j !== 0 && wall.x === current.x && wall.y === current.y) {
           grid[grid.length - 1].push(
-            Object.assign({ isWall: true, isFood: false }, current)
+            Object.assign({
+              isWall: true,
+              isFood: false,
+              isHead: false 
+            }, current)
           );
           continue outer;
         }
       }
       grid[grid.length - 1].push(
-        Object.assign({ isWall: false, isFood: false }, current)
-      );
+        Object.assign({
+          isWall: false, 
+          isFood: false,
+          isHead: false 
+        }, current)
+      )
     }
     return grid;
   }
@@ -46,11 +70,14 @@ const aStar = new function () {
           grid.push([]);
         }
         
-        if (obj.isFood) {
-          grid[grid.length - 1].push(Initial.FOOD);
+        if (obj.isHead) {
+          grid[grid.length - 1].push(Initial.HEAD);
         } 
         else if (obj.isWall) {
           grid[grid.length - 1].push(Initial.WALL);
+        } 
+        else if (obj.isFood) {
+          grid[grid.length - 1].push(Initial.FOOD);
         } 
         else {
           grid[grid.length - 1].push(Initial.AIR);
@@ -67,7 +94,6 @@ const aStar = new function () {
       let start = performance.now();
       this.mapArrayWithPosition = this.convertGridToArray(map, obj.bodies, obj.foods);
       this.mapGrid = this.createGridFromArray(this.mapArrayWithPosition);
-      console.log(performance.now() - start);
     }
     console.log(this.mapGrid);
   }
